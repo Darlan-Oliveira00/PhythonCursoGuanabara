718f3e630fee7811de38408b881b8b4233ee6c65
Darlan Coata de Oliveira <darlancota8@gmail.com>
2025-12-28 10:31:02 -0300

    Create aula2.py

---
+++ GuanabaraPyhthon/mundo2/aula2.py
@@ -0,0 +1,43 @@
+# inicio = int(input('inicio: '))
+# fim = int(input('fim: '))
+# passo = int(input('passo: ')) 
+# for c in range (inicio, fim+1, passo):
+#     print(c)
+# print('FIMMM')
+
+# s = 0
+# for c in range (0, 3):
+#     n = int(input('Digite um número: '))
+#     s += n
+# print('O somatorio de todos os valores foi {}'.format(s))
+# print(f"a soma foi de {s}")
+# print('FIM')
+
+
+# import time
+# for c in range (1,11):
+#     print(c)
+#     time.sleep(1)
+# print('BUMM BUMM')
+
+
+# for c in range (1, 51, 2):
+#     # if c % 2 ==0:
+#     #     print(c)
+#     print(c, end=' ')
+# print('ACABOUUU')
+
+
+# s = 0
+# for c in range (1, 501, 2):
+#     if c % 3 == 0:
+#         s += c
+# print('A soma total é: {}'.format(s))
+
+
+# num = int(input('Digite um número para ver sua tabauda: '))
+# for c in range (1,11): 
+#     r = num * c
+#     print(f'{num} x {c} = {r}')
+#     print('-='*5)
+#     print(f"{num} x {c} = {num*c} ")
