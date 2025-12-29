2ce4ca18baf3ffa5814da982d683e1bac9f4ceae
Darlan Coata de Oliveira <darlancota8@gmail.com>
2025-12-29 11:21:34 -0300

    Update aula2.py

--- GuanabaraPyhthon/mundo2/aula2.py
+++ GuanabaraPyhthon/mundo2/aula2.py
@@ -54,12 +54,12 @@
 
 
 
-print("="*10)
-print('10 TERMOS DE UMA PA')
-print("="*10)
-primeiro = int(input('Primeiro termo: '))
-razao = int(input('Razão: '))
-decimo = primeiro + (10 - 1) * razao
-for c in range (primeiro, decimo + razao, razao):
-    print("{}".format(c), end=" -> ")
-print('ACABOU')
+# print("="*10)
+# print('10 TERMOS DE UMA PA')
+# print("="*10)
+# primeiro = int(input('Primeiro termo: '))
+# razao = int(input('Razão: '))
+# decimo = primeiro + (10 - 1) * razao
+# for c in range (primeiro, decimo + razao, razao):
+#     print("{}".format(c), end=" -> ")
+# print('ACABOU')
