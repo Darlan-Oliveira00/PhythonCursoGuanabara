d75a177ca2591b2553ae4e157a7a3460077582a4
Darlan Coata de Oliveira <darlancota8@gmail.com>
2025-12-29 11:20:43 -0300

    Update aula2.py

--- GuanabaraPyhthon/mundo2/aula2.py
+++ GuanabaraPyhthon/mundo2/aula2.py
@@ -41,3 +41,25 @@
 #     print(f'{num} x {c} = {r}')
 #     print('-='*5)
 #     print(f"{num} x {c} = {num*c} ")
+
+
+# soma = 0
+# for c in range (1, 7):
+#     num = int(input('Digite o {} número: '.format(c)))
+#     if num % 2 == 0:
+#         soma = soma + num
+#     else:
+#         print('O número {} é IMPAR será desconsiderado'.format(num))
+# print("A soma total dos números PARES é: {}".format(soma))
+
+
+
+print("="*10)
+print('10 TERMOS DE UMA PA')
+print("="*10)
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+decimo = primeiro + (10 - 1) * razao
+for c in range (primeiro, decimo + razao, razao):
+    print("{}".format(c), end=" -> ")
+print('ACABOU')
