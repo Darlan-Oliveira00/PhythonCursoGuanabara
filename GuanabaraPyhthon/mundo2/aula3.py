7d769d04ddfaba014408c4d1ac169954ffa2e35e
Darlan Coata de Oliveira <darlancota8@gmail.com>
2026-01-03 17:47:20 -0300

    Update aula3.py

---
+++ GuanabaraPyhthon/mundo2/aula3.py
@@ -0,0 +1,23 @@
+# sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
+# while sexo not in 'MmFf':
+#     sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper()[0]
+# print('Sexo {} registado com sucesso'.format(sexo))
+
+
+from random import randint
+computador = randint(0,10)
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
+print('Será que você consegue adivinhar qual foi?')
+acertou = False
+palpites = 0 
+while not acertou:
+    jogador = int(input('Qual é o seu palpite? '))
+    palpites += 1 
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais... Tente mais uma vez.')
+        elif joagdor > computador:
+            print('Menos... Tente mais uma vez.')
+print('Acertou com {} tentativas. Parabéns!!'.format(palpites))
