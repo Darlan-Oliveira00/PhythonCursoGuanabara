d9db0f4ffe6a7c0aa1b534b19b25ba2a49c3f4db
Darlan Coata de Oliveira <darlancota8@gmail.com>
2026-01-05 16:11:10 -0300

    Update aula4.py

--- GuanabaraPyhthon/mundo2/aula4.py
+++ GuanabaraPyhthon/mundo2/aula4.py
@@ -61,8 +61,34 @@
 
 
 
-while True: 
-    idade = int(input('Idade: '))
-    sexo = ' '
-    while sexo sexo not in  'MF:'
-        sexo = str(input('Sexo: [M/F]')).strip().upper()[0]
+# tot18 = tothomens = totM20 =  0
+# while True: 
+#     idade = int(input('Idade: '))
+#     sexo = ' '
+#     while sexo not in  'MF':
+#         sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
+#     if idade >= 18: 
+#          tot18 += 1
+#     if sexo == 'M':
+#          tothomens += 1
+#     if sexo == 'F' and idade < 20:
+#          totM20 += 1
+#     resp = ' '
+#     while resp not in 'SN':
+#         resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+#     if resp == 'N':
+#             break
+# print(f'Total de pessoas com mais de 18 anos: {tot18}')
+# print(f'Total de Homens: {tothomens}')
+# print(f'Total de Mulheres com menos de 20 anos é {totM20}')
+
+
+
+
+while True:
+    produto = str(input('Nome do Produto: '))
+    preco = float(input('Preço: R$'))
+
+    resp = ' '
+    while not in 'SN':
+        resp = str(input('Quer continuar? [S/N]')).strip().upper()[0]
