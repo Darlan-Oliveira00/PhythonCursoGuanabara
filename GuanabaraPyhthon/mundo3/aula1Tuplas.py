732e6b60f5ff0d62381c7dc725ffdaeaf03ec1d2
Darlan Coata de Oliveira <darlancota8@gmail.com>
2026-01-08 17:18:22 -0300

    Update aula1Tuplas.py

--- GuanabaraPyhthon/mundo3/aula1Tuplas.py
+++ GuanabaraPyhthon/mundo3/aula1Tuplas.py
@@ -1,4 +1,4 @@
-lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+# lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
 # # for cont in range (0, len(lanche)):
 # #     print(f'Eu vou comer {lanche[cont]} na posição {cont}')
 
