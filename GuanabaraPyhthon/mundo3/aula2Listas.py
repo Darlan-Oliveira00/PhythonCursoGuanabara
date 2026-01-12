3789055e1d33a94b5e9f5997092654fb83a65ae1
Darlan Coata de Oliveira <darlancota8@gmail.com>
2026-01-12 16:17:51 -0300

    segunda parte do assunto de listas

--- GuanabaraPyhthon/mundo3/aula2Listas.py
+++ GuanabaraPyhthon/mundo3/aula2Listas.py
@@ -126,5 +126,21 @@
 # print(f'Os pares são {pares}')
 
 
-expr = str(input('Digite a expressão: '))
-pilha = list()
+# expr = str(input('Digite a expressão: '))
+# pilha = list()
+# for simb in expr:
+#     if simb == '(':
+#         pilha.append('(')
+#     elif simb == ')':
+#         if len(pilha) > 0:
+#             pilha.pop()
+#         else:
+#             pilha.append(')')
+#             break 
+# if len(pilha) == 0:
+#     print('Sua expresão está válida!')
+# else:
+#     print('Sua expressão está errada!')
+
+
+
