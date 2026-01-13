76ab2493bc5828ae2f38856c914004b3f8b04f2e
Darlan Coata de Oliveira <darlancota8@gmail.com>
2026-01-13 16:53:23 -0300

    Update aula3ListasP2.py

--- GuanabaraPyhthon/mundo3/aula3ListasP2.py
+++ GuanabaraPyhthon/mundo3/aula3ListasP2.py
@@ -69,3 +69,11 @@
 
 num = [[], []]
 valor = 0 
+for c in range (1,8):
+    valor = int(input(f'Digite o {c} valor: '))
+    if valor % 2 == 0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+print('-='*40)
+print(f'Todos os valores {num}')
