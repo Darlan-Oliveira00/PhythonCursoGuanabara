6a63a913330c14237b7da109c124763714bd04dc
Darlan Coata de Oliveira <darlancota8@gmail.com>
2026-01-13 17:36:44 -0300

    Update aula3ListasP2.py

--- GuanabaraPyhthon/mundo3/aula3ListasP2.py
+++ GuanabaraPyhthon/mundo3/aula3ListasP2.py
@@ -67,13 +67,57 @@
 
 
 
-num = [[], []]
-valor = 0 
-for c in range (1,8):
-    valor = int(input(f'Digite o {c} valor: '))
-    if valor % 2 == 0:
-        num[0].append(valor)
-    else:
-        num[1].append(valor)
-print('-='*40)
-print(f'Todos os valores {num}')
+# num = [[], []]
+# valor = 0 
+# for c in range (1,8):
+#     valor = int(input(f'Digite o {c} valor: '))
+#     if valor % 2 == 0:
+#         num[0].append(valor)
+#     else:
+#         num[1].append(valor)
+# print('-='*40)
+# num[0].sort()
+# num[1].sort()
+# print(f'Os valores pares digitados foram: {num[0]}')
+# print(f'Os valores ímpares digitados foram {num[1]}')
+
+
+# matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+# for l in range (0,3):
+#     for c in range (0,3):
+#         matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
+# print('-='*30)
+# for l in range (0,3):
+#     for c in range (0,3):
+#         print(f'[{matriz[l] [c]:^5}]', end='')
+#     print()
+
+
+
+# matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+# spar = mai = scol = 0 
+# for l in range (0,3):
+#     for c in range (0,3):
+#         matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
+# print('-='*30)
+# for l in range (0,3):
+#     for c in range (0,3):
+#         print(f'[{matriz[l] [c]:^5}]', end='')
+#         if matriz[l][c] % 2 == 0:
+#             spar += matriz[l][c]
+#     print()
+# print('-='*30)
+# print(f'A soma dos valores é {spar}')
+# for l in range (0, 3):
+#     scol += matriz[l][2]
+# print(f'A soma dos valores da terceira coluna é {scol}.')
+# for c in range (0,3):
+#     if c == 0: 
+#         mai = matriz[1][c]
+#     elif matriz[1][c] > mai:
+#         mai = matriz[1][c]
+# print(f'O maior valor da segunda linha é {mai}.')
+
+
+
+from random import randint
