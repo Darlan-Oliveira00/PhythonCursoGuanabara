a41aa4875ede214d8a0fca6e13374ae0bd110f55
Darlan Coata de Oliveira <darlancota8@gmail.com>
2026-01-17 08:51:21 -0300

    Update aula3ListasP2.py

--- GuanabaraPyhthon/mundo3/aula3ListasP2.py
+++ GuanabaraPyhthon/mundo3/aula3ListasP2.py
@@ -1,148 +1,172 @@
-# teste = list()
-# teste.append('Gustavo')
-# teste.append(40)
-# galera = list()
-# galera.append(teste[:])
-# teste[0] = 'Maria'
-# teste[1] = 22
-# galera.append(teste[:])
-# print(galera)
+teste = list()
+teste.append('Gustavo')
+teste.append(40)
+galera = list()
+galera.append(teste[:])
+teste[0] = 'Maria'
+teste[1] = 22
+galera.append(teste[:])
+print(galera)
 
 
 
-# galera = [['João', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
-# for p in galera:
-#     print(f'{p[0]} tem {p[1]} anos de idade.')
+galera = [['João', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
+for p in galera:
+    print(f'{p[0]} tem {p[1]} anos de idade.')
 
-# galera = list()
-# dado = list()
-# totmaior = totmenor = 0 
-# for c in range (0, 3):
-#     dado.append(str(input('Nome: ')))
-#     dado.append(int(input('Idade: ')))
-#     galera.append(dado[:])
-#     dado.clear()
-# for p in galera:
-#     if p[1] >= 21:
-#         print(f'{p[0]} é maior de idade')
-#         totmaior += 1
-#     else:
-#         print(f'{p[0]} é menor de idade')
-#         totmenor += 1
-# print(f'Temos {totmaior} maiores de idade e {totmenor} menores de idade.')
-
-
-
-# temp = []
-# princ = []
-# maior = menor = 0 
-# while True:
-#     temp.append(str(input('Nome: ')))
-#     temp.append(float(input('Peso: ')))
-#     if len(princ) == 0:
-#         maior = menor = temp[1]
-#     else:
-#         if temp[1] > maior:
-#             maior = temp[1]
-#         if temp[1] < menor:
-#             menor = temp[1]
-#     princ.append(temp[:])
-#     temp.clear()
-#     resposta = str(input('QUER CONTINUAR? [S/N] '))
-#     if resposta in 'Nn':
-#         break
-# print('-='*30)
-# print(princ)
-# print(f'Ao todo você cadrastrou {len(princ)} pessoas.')
-# print(f'O maior pesso é {maior}. Peso de ', end='')
-# for p in princ:
-#     if p[1] == maior:
-#         print(f'{p[0]} ', end='')
-# print()
-# print(f'O menor pesso é {menor}', end='')
-# for p in princ:
-#     if p[1] == menor:
-#         print(f'{p[0]}', end='')
-# print()
+galera = list()
+dado = list()
+totmaior = totmenor = 0 
+for c in range (0, 3):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    galera.append(dado[:])
+    dado.clear()
+for p in galera:
+    if p[1] >= 21:
+        print(f'{p[0]} é maior de idade')
+        totmaior += 1
+    else:
+        print(f'{p[0]} é menor de idade')
+        totmenor += 1
+print(f'Temos {totmaior} maiores de idade e {totmenor} menores de idade.')
 
 
 
-# num = [[], []]
-# valor = 0 
-# for c in range (1,8):
-#     valor = int(input(f'Digite o {c} valor: '))
-#     if valor % 2 == 0:
-#         num[0].append(valor)
-#     else:
-#         num[1].append(valor)
-# print('-='*40)
-# num[0].sort()
-# num[1].sort()
-# print(f'Os valores pares digitados foram: {num[0]}')
-# print(f'Os valores ímpares digitados foram {num[1]}')
-
-
-# matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-# for l in range (0,3):
-#     for c in range (0,3):
-#         matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
-# print('-='*30)
-# for l in range (0,3):
-#     for c in range (0,3):
-#         print(f'[{matriz[l] [c]:^5}]', end='')
-#     print()
-
-
-
-# matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-# spar = mai = scol = 0 
-# for l in range (0,3):
-#     for c in range (0,3):
-#         matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
-# print('-='*30)
-# for l in range (0,3):
-#     for c in range (0,3):
-#         print(f'[{matriz[l] [c]:^5}]', end='')
-#         if matriz[l][c] % 2 == 0:
-#             spar += matriz[l][c]
-#     print()
-# print('-='*30)
-# print(f'A soma dos valores é {spar}')
-# for l in range (0, 3):
-#     scol += matriz[l][2]
-# print(f'A soma dos valores da terceira coluna é {scol}.')
-# for c in range (0,3):
-#     if c == 0: 
-#         mai = matriz[1][c]
-#     elif matriz[1][c] > mai:
-#         mai = matriz[1][c]
-# print(f'O maior valor da segunda linha é {mai}.')
+temp = []
+princ = []
+maior = menor = 0 
+while True:
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+    if len(princ) == 0:
+        maior = menor = temp[1]
+    else:
+        if temp[1] > maior:
+            maior = temp[1]
+        if temp[1] < menor:
+            menor = temp[1]
+    princ.append(temp[:])
+    temp.clear()
+    resposta = str(input('QUER CONTINUAR? [S/N] '))
+    if resposta in 'Nn':
+        break
+print('-='*30)
+print(princ)
+print(f'Ao todo você cadrastrou {len(princ)} pessoas.')
+print(f'O maior pesso é {maior}. Peso de ', end='')
+for p in princ:
+    if p[1] == maior:
+        print(f'{p[0]} ', end='')
+print()
+print(f'O menor pesso é {menor}', end='')
+for p in princ:
+    if p[1] == menor:
+        print(f'{p[0]}', end='')
+print()
 
 
 
-# from random import randint
-# lista = list()
-# jogos = list()
-# print('-='*20)
-# print('       JOGA NA MEGA SENA        ')
-# print('-='*20)
-# quant = int (input('Quantos jogos você quer que eu sorteie? '))
-# tot = 1
-# while tot <= quant:
-#     cont = 0
-#     while True:
-#         num = randint(1, 60)
-#         if num not in lista:
-#             lista.append(num)
-#             cont += 1
-#         if cont >= 6:
-#             break
-#     lista.sort()
-#     jogos.append(list[:])
-#     lista.clear
-#     tot += 1
-# print(f'Os números sorteados foram {lista}')
+num = [[], []]
+valor = 0 
+for c in range (1,8):
+    valor = int(input(f'Digite o {c} valor: '))
+    if valor % 2 == 0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+print('-='*40)
+num[0].sort()
+num[1].sort()
+print(f'Os valores pares digitados foram: {num[0]}')
+print(f'Os valores ímpares digitados foram {num[1]}')
+
+
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range (0,3):
+    for c in range (0,3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
+print('-='*30)
+for l in range (0,3):
+    for c in range (0,3):
+        print(f'[{matriz[l] [c]:^5}]', end='')
+    print()
+
+
+
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+spar = mai = scol = 0 
+for l in range (0,3):
+    for c in range (0,3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
+print('-='*30)
+for l in range (0,3):
+    for c in range (0,3):
+        print(f'[{matriz[l] [c]:^5}]', end='')
+        if matriz[l][c] % 2 == 0:
+            spar += matriz[l][c]
+    print()
+print('-='*30)
+print(f'A soma dos valores é {spar}')
+for l in range (0, 3):
+    scol += matriz[l][2]
+print(f'A soma dos valores da terceira coluna é {scol}.')
+for c in range (0,3):
+    if c == 0: 
+        mai = matriz[1][c]
+    elif matriz[1][c] > mai:
+        mai = matriz[1][c]
+print(f'O maior valor da segunda linha é {mai}.')
+
+
+
+from random import randint
+lista = list()
+jogos = list()
+print('-='*20)
+print('       JOGA NA MEGA SENA        ')
+print('-='*20)
+quant = int (input('Quantos jogos você quer que eu sorteie? '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(list[:])
+    lista.clear
+    tot += 1
+print(f'Os números sorteados foram {lista}')
 
 
 
 
+ficha = list()
+while True:
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], media])
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+print('-='*30)
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
+print('-'*26)
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('--'*35)
+    opc = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
+    if opc == 999:
+        print('FINALIZANDO...')
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
+print('<<<<<<< VOLTE SEMPRE >>>>>>>>>')
