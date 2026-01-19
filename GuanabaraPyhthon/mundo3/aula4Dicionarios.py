e5acb3c956e3f44c540d5ae8d41ff76bea9554f2
Darlan Coata de Oliveira <darlancota8@gmail.com>
2026-01-19 17:00:53 -0300

    Update aula4Dicionarios.py

--- GuanabaraPyhthon/mundo3/aula4Dicionarios.py
+++ GuanabaraPyhthon/mundo3/aula4Dicionarios.py
@@ -1,9 +1,17 @@
-pessoas = {'nome': 'Gustavo', 'sexo': 'M', 'idade': 22}
+# pessoas = {'nome': 'Gustavo', 'sexo': 'M', 'idade': 22}
 # print(F'O {pessoas["nome"]} tem {pessoas["idade"]} anos')
 # print(pessoas.items())
 # print(pessoas.values())
 # print(pessoas.keys())
 # del pessoas['sexo']
-pessoas['pesso'] = 98.5
-for k, v in pessoas.items():
-    print(f'{k} = {v}')
+# pessoas['pesso'] = 98.5
+# for k, v in pessoas.items():
+#     print(f'{k} = {v}')
+
+brasil = []
+estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+estado2 = {'uf': 'São Paulo', 'Sigla': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+
+print(brasil)
