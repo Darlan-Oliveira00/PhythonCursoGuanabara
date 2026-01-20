37f8e0979f386f077c7215d69234ad9c26a4b8e0
Darlan Coata de Oliveira <darlancota8@gmail.com>
2026-01-20 15:13:41 -0300

    Update aula4Dicionarios.py

--- GuanabaraPyhthon/mundo3/aula4Dicionarios.py
+++ GuanabaraPyhthon/mundo3/aula4Dicionarios.py
@@ -10,8 +10,9 @@
 
 brasil = []
 estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
-estado2 = {'uf': 'São Paulo', 'Sigla': 'SP'}
+estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
 brasil.append(estado1)
 brasil.append(estado2)
 
 print(brasil)
+print(brasil[1]['sigla'])
