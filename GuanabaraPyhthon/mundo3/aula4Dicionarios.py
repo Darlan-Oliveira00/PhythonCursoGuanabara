85a16a7dca68c930348fe7fad329b21292e00a85
Darlan Coata de Oliveira <darlancota8@gmail.com>
2026-01-21 15:12:31 -0300

    Update aula4Dicionarios.py

--- GuanabaraPyhthon/mundo3/aula4Dicionarios.py
+++ GuanabaraPyhthon/mundo3/aula4Dicionarios.py
@@ -8,11 +8,43 @@
 # for k, v in pessoas.items():
 #     print(f'{k} = {v}')
 
-brasil = []
-estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
-estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
-brasil.append(estado1)
-brasil.append(estado2)
+# brasil = []
+# estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+# estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
+# brasil.append(estado1)
+# brasil.append(estado2)
 
-print(brasil)
-print(brasil[1]['sigla'])
+# print(brasil)
+# print(brasil[1]['sigla'])
+
+
+# from random import randint
+# from time import sleep
+# from _operator import itemgetter
+# jogo = {'jogador 1':randint(1,6),
+#         'jogador 2':randint(1,6),
+#         'jogador 3':randint(1,6),
+#         'jogador 4':randint(1,6)} 
+# print('Valores sorteados: ')
+# ranking = list()
+# for k, v in jogo.items():
+#     print(f'{k} tirou {v} no dado')
+#     sleep(1)
+# ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+# for i, v in enumerate(ranking):
+#     print(f'{i+1} lugar: {v[0]} com {v[1]}')
+#     sleep(1)
+
+
+from datetime import datetime
+dados = dict()
+dados['nome'] = str(input('Nome: '))
+nasc = int(input('Ano de Nascimento: '))
+dados['idade'] = datetime.now().year -  nasc
+dados['ctps'] = int(input('Carteira de Trabalho: (0 não tem): '))
+if dados['ctps']!= 0:
+    dados['contração'] = int(input('Ano de Contratação: '))
+    dados['Salario'] = float(input('Salário: R$'))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratação'] + 35) - datetime.now().year)
+print('-='*30)
+print(dados)
